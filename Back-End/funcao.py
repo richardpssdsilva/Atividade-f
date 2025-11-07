6e9c6432a2e425027948043f1d1f2186e04d6232
Richard <richardpssdsilva@gmail.com>
2025-11-07 20:01:30 -0300

    listar produtos

--- Back-End/funcao.py
+++ Back-End/funcao.py
@@ -15,3 +15,17 @@ def adicionar_produto(nome, preco):
             cursor.close()
             conexao.commit()
             
+def listar_produtos():
+    conexao, cursor = conector()
+    if conexao: 
+        try: 
+            cursor.execute(
+                "SELECT * FROM produtos ORDER BY id"
+                )
+            return cursor.fetchall()
+        except Exception as erro: 
+            print(f"Erro ao listar os produtos {erro}")
+            return []
+        finally:
+            cursor.close()
+            conexao.commit()
