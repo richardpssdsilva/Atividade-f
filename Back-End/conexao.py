7928f744c1e3d6aa5fa951abf0a8525b444d4c94
Richard <richardpssdsilva@gmail.com>
2025-11-07 19:54:22 -0300

    first commit

---
+++ Back-End/conexao.py
@@ -0,0 +1,25 @@
+import psycopg2 as pg 
+from dotenv import load_dotenv
+import os
+
+#Carregar .env
+load_dotenv()
+
+
+def conector():
+    try:
+        conexao = pg(
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexao: {erro}")
+        return None, None
+
+conector()
+    
